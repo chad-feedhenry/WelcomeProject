756fe5caee7516c1ac8f86b761201b9e9597d1f8
Conor ONeill <conor.oneill@feedhenry.com>
2013-10-02 18:45:44 +0100

    tiny text change

--- client/app/scripts/models/page.js
+++ client/app/scripts/models/page.js
@@ -46,8 +46,8 @@ App.models.databrowserPage = new App.Model.PageModel({
 });
 
 App.models.weatherPage = new App.Model.PageModel({
-  title: 'Location and Weather',
-  header: 'Location and Weather Example',
+  title: 'Location Example',
+  header: 'Location Example',
   geoText: 'Let’s grab your location and do something with it. Tap the button to find your location',
   geoButtonText : 'Get My Location',
   geoLabelText: 'My Location',
